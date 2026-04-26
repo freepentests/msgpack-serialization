e502e3a3139001f46b32f545df70410684802a07
freepentests <252639873+freepentests@users.noreply.github.com>
2026-04-26 22:24:32 +0100

    made the code a bit more readable

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This is an implementation of the MessagePack serialization format. Read the [Mes
 I decided to write this MessagePack implementation for my moomoo.io script, since moomoo.io uses MessagePack for efficient network communication.
 At the moment, my moomoo.io script uses a msgpack library I found on NPM, but I want to use my own libraries, since using libraries created by other people is like skidding in a way.
 
+### extension types are not yet supported
+
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import TypeIdentifiers from './Modules/TypeIdentifiers.js';
+
 // I know a lot of this code is poor quality, but this is a work in progress, and I will be refactoring it; at the moment, I'm just focusing on getting something that works
 
 // isFloat polyfill
@@ -18,7 +20,7 @@ class MsgpackEncoder {
 
 		return this.byteArray.slice(0, this.index);
 	}
-	
+
 	appendData(data) {
 		switch (typeof data) {
 			case 'boolean':
@@ -42,43 +44,81 @@ class MsgpackEncoder {
 		}
 	}
 
-	appendNull() {
-		this.appendByte(0xc0);
+	createByteArray(bufferSize, dataViewFunction, data) {
+		const buffer = new ArrayBuffer(bufferSize);
+		const dataView = new DataView(buffer);
+
+		dataView[dataViewFunction](0, data);
+
+		const byteArray = new Uint8Array(buffer);
+
+		return byteArray;
 	}
 
-	appendArray(array) {
-		const isFixArray = array.length >= 0 && array.length < 15
+
+	appendNull() {
+		this.appendByte(TypeIdentifiers.nil);
+	}
+
+	appendFixArrayLength(array) {
+		this.appendByte(TypeIdentifiers.fixArray + array.length);
+	}
+
+	append16ByteArrayLength(array) {
+		this.appendByte(TypeIdentifiers.var_16ByteLengthArray);
+
+		const encodedArrayLength = this.createByteArray(2, 'setUint16', array.length)
+		this.appendBytes(encodedArrayLength);
+	}
+
+	append32ByteArrayLength(array) {
+		this.appendByte(TypeIdentifiers.var_32ByteLengthArray);
+
+		const encodedArrayLength = this.createByteArray(4, 'setUint32', array.length);
+		this.appendBytes(encodedArrayLength);
+	}
+
+	appendArrayLength(array) {
+		const isFixArray = array.length >= 0 && array.length < 15;
 		const is16ByteLengthArray = array.length >= 15 && array.length < (2 ** 16) - 1;
 		const is32ByteLengthArray = array.length >= (2 ** 16) && array.length <= (2 ** 32) - 1;
 
 		if (isFixArray) {
-			this.appendByte(0x90 + array.length);
+			this.appendFixArrayLength(array)
 		} else if (is16ByteLengthArray) {
-			this.appendByte(0xdc);
-
-			const buffer = new ArrayBuffer(2);
-			const dataView = new DataView(buffer);
-			dataView.setUint16(0, array.length);
-			const encodedArrayLength = new Uint8Array(buffer);
-
-			this.appendBytes(encodedArrayLength);
+			this.append16ByteArrayLength(array);
 		} else if (is32ByteLengthArray) {
-			this.appendByte(0xdd);
-
-			const buffer = new ArrayBuffer(4);
-			const dataView = new DataView(buffer);
-			dataView.setUint32(0, array.length);
-			const encodedArrayLength = new Uint8Array(buffer);
-
-			this.appendBytes(encodedArrayLength);
+			this.append32ByteArrayLength(array);
 		}
+	}
+
+	appendArray(array) {
+		this.appendArrayLength(array);
 
 		array.forEach((element) => {
 			this.appendData(element);
 		});
 	}
 
-	appendObject(object) {
+	appendFixMapLength(objectLength) {
+		this.appendByte(TypeIdentifiers.fixMap + objectLength);
+	}
+
+	append16ByteMapLength(objectLength) {
+		this.appendByte(TypeIdentifiers.var_16ByteMap);
+
+		const encodedObjectLength = this.createByteArray(2, 'setUint16', objectLength);
+		this.appendBytes(encodedObjectLength);
+	}
+
+	append32ByteMapLength(objectLength) {
+		this.appendByte(TypeIdentifiers.var_32ByteMap);
+
+		const encodedObjectLength = this.createByteArray(4, 'setUint32', objectLength);
+		this.appendBytes(encodedObjectLength);
+	}
+
+	appendObjectLength(object) {
 		const objectKeys = Object.keys(object);
 		const objectLength = objectKeys.length;
 
@@ -87,76 +127,116 @@ class MsgpackEncoder {
 		const is32ByteMap = objectLength >= (2 ** 16) - 1 && objectLength < (2 ** 32) - 1;
 
 		if (isFixMap) {
-			this.appendByte(0x80 + objectLength);
+			this.appendFixMapLength(objectLength);
 		} else if (is16ByteMap) {
-			this.appendByte(0xde);
-
-			const buffer = new ArrayBuffer(2);
-			const dataView = new DataView(buffer);
-			dataView.setUint16(0, objectLength)
-			const encodedObjectLength = new Uint8Array(buffer);
-
-			this.appendBytes(encodedObjectLength);
+			this.append16ByteMapLength(objectLength);
 		} else if (is32ByteMap) {
-			this.appendByte(0xdf);
-
-			const buffer = new ArrayBuffer(2);
-			const dataView = new DataView(buffer);
-			dataView.setUint32(0, objectLength)
-			const encodedObjectLength = new Uint8Array(buffer);
-
-			this.appendBytes(encodedObjectLength);
+			this.append32ByteMapLength(objectLength);
 		}
+	}
 
-		for (const key of objectKeys) {
+	appendObject(object) {
+		this.appendObjectLength(object);
+
+		for (const key of Object.keys(object)) {
 			this.appendData(key);
 			this.appendData(object[key]);
 		}
 	}
 
-	appendString(string) {
-		const isFixStr = string.length > 0 && string.length < 32;
-		const is8ByteLengthStr = string.length >= 32 && string.length < (2 ** 8) - 1;
-		const is16ByteLengthStr = string.length >= (2 ** 8) - 1 && string.length < (2 ** 16) - 1;
-		const is32ByteLengthStr = string.length >= (2 ** 16) - 1 && string.length < (2 ** 32) - 1;
+	appendFixStringLength(stringLength) {
+		this.appendByte(TypeIdentifiers.fixStr + stringLength);
+	}
+
+	append8ByteStringLength(stringLength) {
+		this.appendBytes([TypeIdentifiers.var_8ByteLengthStr, stringLength]);
+	}
+
+	append16ByteStringLength(stringLength) {
+		this.appendByte(TypeIdentifiers.var_16ByteLengthStr);
+
+		const encodedStringLength = this.createByteArray(2, 'setUint16', stringLength);
+		this.appendBytes(encodedStringLength);
+	}
+
+	append32ByteStringLength(stringLength) {
+		this.appendByte(TypeIdentifiers.var_16ByteLengthStr);
+
+		const encodedStringLength = this.createByteArray(4, 'setUint32', stringLength);
+		this.appendBytes(encodedStringLength);
+	}
+
+	appendStringLength(stringLength) {
+		const isFixStr = stringLength > 0 && stringLength < 32;
+		const is8ByteLengthStr = stringLength >= 32 && stringLength < (2 ** 8) - 1;
+		const is16ByteLengthStr = stringLength >= (2 ** 8) - 1 && stringLength < (2 ** 16) - 1;
+		const is32ByteLengthStr = stringLength >= (2 ** 16) - 1 && stringLength < (2 ** 32) - 1;
 
 		if (isFixStr) {
-			this.appendByte(0xa0 + string.length);
-
-			const encodedStringData = new TextEncoder().encode(string);
-			this.appendBytes(encodedStringData);
-
+			this.appendFixStringLength(stringLength);
 		} else if (is8ByteLengthStr) {
-			this.appendBytes([0xd9, string.length]);
-
-			const encodedStringData = new TextEncoder().encode(string);
-			this.appendBytes(encodedStringData);
-
+			this.append8ByteStringLength(stringLength);
 		} else if (is16ByteLengthStr) {
-			this.appendByte(0xda);
-
-			const buffer = new ArrayBuffer(2);
-			const dataView = new DataView(buffer);
-			dataView.setUint16(0, string.length);
-			const encodedStringLength = new Uint8Array(buffer);
-
-			this.appendBytes(encodedStringLength);
-			const encodedStringData = new TextEncoder().encode(string);
-
-			this.appendBytes(encodedStringData);
+			this.append16ByteStringLength(stringLength);
 		} else if (is32ByteLengthStr) {
-			this.appendByte(0xdb);
-
-			const buffer = new ArrayBuffer(4);
-			const dataView = new DataView(buffer);
-			dataView.setUint32(0, string.length);
-			const encodedStringLength = new Uint8Array(buffer);
-
-			this.appendBytes(encodedStringLength);
-			const encodedStringData = new TextEncoder().encode(string);
-
-			this.appendBytes(encodedStringData);
+			this.append32ByteStringLength(stringLength);
 		}
+	}
+
+	appendString(string) {
+		this.appendStringLength(string.length);
+
+		const encodedStringData = new TextEncoder().encode(string);
+		this.appendBytes(encodedStringData);
+	}
+
+	append8BitUnsignedInt(number) {
+		this.appendByte(TypeIdentifiers.var_8BitUnsignedInt);
+		this.appendByte(number);
+	}
+
+	append16BitUnsignedInt(number) {
+		this.appendByte(TypeIdentifiers.var_16BitUnsignedInt);
+
+		const encodedInteger = this.createByteArray(2, 'setUint16', number);
+		this.appendBytes(encodedInteger);
+	}
+
+	append32BitUnsignedInt(number) {
+		this.appendByte(TypeIdentifiers.var_32BitUnsignedInt);
+
+		const encodedInteger = this.createByteArray(4, 'setUint32', number);
+		this.appendBytes(encodedInteger);
+	}
+
+	append64BitUnsignedInt(number) {
+		this.appendByte(TypeIdentifiers.var_32BitUnsignedInt);
+
+		const encodedInteger = this.createByteArray(8, 'setUint64', number);
+		this.appendBytes(encodedInteger);
+	}
+
+	append8BitSignedInt(number) {
+		this.appendByte(TypeIdentifiers.var_8BitSignedInt);
+		this.appendByte(number);
+	}
+
+	append16BitSignedInt(number) {
+		this.appendByte(TypeIdentifiers.var_16BitSignedInt);
+		const encodedInteger = this.createByteArray(2, 'setInt16', number);
+		this.appendBytes(encodedInteger);
+	}
+
+	append32BitSignedInt(number) {
+		this.appendByte(TypeIdentifiers.var_32BitSignedInt);
+		const encodedInteger = this.createByteArray(4, 'setInt32', number);
+		this.appendBytes(encodedInteger);
+	}
+
+	append64BitSignedInt(number) {
+		this.appendByte(TypeIdentifiers.var_64BitSignedInt);
+		const encodedInteger = this.createByteArray(8, 'setInt64', number);
+		this.appendBytes(encodedInteger);
 	}
 
 	appendInt(number) {
@@ -174,104 +254,31 @@ class MsgpackEncoder {
 		const is64BitSignedInt = number >= -(2 ** 65) - 1 && number < (2 ** 65);
 
 		if (isUnsignedFixint || isSignedFixint) {
-			this.appendByte(number);
-			return;
-		}
-
-		if (is8BitUnsignedInt) {
-			this.appendByte(0xcc);
-			this.appendByte(number);
-			return;
-		}
-
-		if (is16BitUnsignedInt) {
-			this.appendByte(0xcd);
-
-			const buffer = new ArrayBuffer(2);
-			const dataView = new DataView(buffer);
-			dataView.setUint16(0, number);
-			const encodedByteArray = new Uint8Array(buffer);
-
-			this.appendBytes(encodedByteArray);
-			return;
-		}
-
-		if (is32BitUnsignedInt) {
-			this.appendByte(0xce);
-
-			const buffer = new ArrayBuffer(4);
-			const dataView = new DataView(buffer);
-			dataView.setUint32(0, number);
-			const encodedByteArray = new Uint8Array(buffer);
-
-			this.appendBytes(encodedByteArray);
-			return;
-		}
-		
-		if (is64BitUnsignedInt) {
-			this.appendByte(0xcf);
-
-			const buffer = new ArrayBuffer(8);
-			const dataView = new DataView(buffer);
-			dataView.setUint64(0, number);
-			const encodedByteArray = new Uint8Array(buffer);
-
-			this.appendBytes(encodedByteArray);
-			return;
-		}
-
-		if (is8BitSignedInt) {
-			this.appendByte(0xd0);
-			this.appendByte(number);
-			return;
-		}
-
-		if (is16BitSignedInt) {
-			this.appendByte(0xd1);
-
-			const buffer = new ArrayBuffer(2);
-			const dataView = new DataView(buffer);
-			dataView.setInt16(0, number);
-			const encodedByteArray = new Uint8Array(buffer);
-
-			this.appendBytes(encodedByteArray);
-			return;
-		}
-
-		if (is32BitSignedInt) {
-			this.appendByte(0xd2);
-
-			const buffer = new ArrayBuffer(4);
-			const dataView = new DataView(buffer);
-			dataView.setInt32(0, number);
-			const encodedByteArray = new Uint8Array(buffer);
-
-			this.appendBytes(encodedByteArray);
-			return;
-		}
-		
-		if (is64BitSignedInt) {
-			this.appendByte(0xd3);
-
-			const buffer = new ArrayBuffer(8);
-			const dataView = new DataView(buffer);
-			dataView.setInt64(0, number);
-			const encodedByteArray = new Uint8Array(buffer);
-
-			this.appendBytes(encodedByteArray);
-			return;
+			return this.appendByte(number);
+		} else if (is8BitUnsignedInt) {
+			return this.append8BitUnsignedInt(number);
+		} else if (is16BitUnsignedInt) {
+			return this.append16BitUnsignedInt(number);
+		} else if (is32BitUnsignedInt) {
+			return this.append32BitUnsignedInt(number);
+		} else if (is64BitUnsignedInt) {
+			return this.append64BitUnsignedInt(number);
+		} else if (is8BitSignedInt) {
+			return this.append8BitSignedInt(number);
+		} else if (is16BitSignedInt) {
+			return this.append16BitSignedInt(number);
+		} else if (is32BitSignedInt) {
+			return this.append32BitSignedInt(number);
+		} else if (is64BitSignedInt) {
+			return this.append64BitSignedInt(number);
 		}
 	}
 
 	appendFloat(number) {
-		this.appendByte(0xcb);
+		this.appendByte(TypeIdentifiers.float64);
 
-		const buffer = new ArrayBuffer(8);
-		const dataView = new DataView(buffer);
-		dataView.setFloat64(0, number);
-		const encodedByteArray = new Uint8Array(buffer);
-
-		this.appendBytes(encodedByteArray);
+		const encodedInteger = this.createByteArray(8, 'setFloat64', number);
+		this.appendBytes(encodedInteger);
 	}
 
 	appendNumber(number) {
@@ -282,56 +289,64 @@ class MsgpackEncoder {
 		}
 	}
 
+	append8ByteBinArray(binArray) {
+		this.appendBytes([TypeIdentifiers.var_8ByteLengthBinArray, binArray.length]);
+	}
+
+	append16ByteBinArray(binArray) {
+		this.appendByte(TypeIdentifiers.var_16ByteLengthBinArray);
+		const encodedBinArrayLength = this.createByteArray(2, 'setUint16', binArray.length);
+		this.appendBytes(encodedBinArrayLength);
+	}
+
+	append32ByteBinArray(binArray) {
+		this.appendByte(TypeIdentifiers.var_32ByteLengthBinArray);
+		const encodedBinArrayLength = this.createByteArray(4, 'setUint32', binArray.length);
+		this.appendBytes(encodedBinArrayLength);
+	}
+
 	appendBinArray(binArray) {
 		const is8ByteLengthBinArray = binArray.length >= 0 && binArray.length < (2 ** 8) - 1;
 		const is16ByteLengthBinArray = binArray.length >= (2 ** 8) - 1  && binArray.length < (2 ** 16) - 1;
 		const is32ByteLengthBinArray = binArray.length >= (2 ** 16) - 1  && binArray.length < (2 ** 32) - 1;
 
 		if (is8ByteLengthBinArray) {
-			this.appendBytes([0xc4, binArray.length]);
-			this.appendBytes(binArray);
+			this.append8ByteBinArray(binArray);
 		} else if (is16ByteLengthBinArray) {
-			this.appendByte(0xc5);
-
-			const buffer = new ArrayBuffer(2);
-			const dataView = new DataView(buffer);
-			dataView.setUint16(0, binArray.length);
-			const encodedByteArray = new Uint8Array(buffer);
-
-			this.appendBytes(encodedByteArray);
-			this.appendBytes(binArray);
+			this.append16ByteBinArray(binArray);
 		} else if (is32ByteLengthBinArray) {
-			this.appendByte(0xc6);
-
-			const buffer = new ArrayBuffer(4);
-			const dataView = new DataView(buffer);
-			dataView.setUint32(0, binArray.length);
-			const encodedByteArray = new Uint8Array(buffer);
-
-			this.appendBytes(encodedByteArray);
-			this.appendBytes(binArray);
+			this.append32ByteBinArray(binArray);
 		}
 	}
 
 	appendBool(bool) {
-		if (bool) this.appendByte(0xc2);
-		else this.appendByte(0xc3);
+		if (bool) this.appendByte(TypeIdentifiers.trueBool);
+		else this.appendByte(TypeIdentifiers.falseBool);
+	}
+
+	increaseByteArraySize() {
+		const oldArray = this.byteArray; 
+
+		const newLength = oldArray.length * 2;
+		const newArray = new Uint8Array(newLength);
+
+		newArray.set(oldArray);
+
+		this.byteArray = newArray;
+	}
+
+	setByteAtCurrentIndex(byte) {
+		this.byteArray[this.index] = byte;
+		this.index++;
 	}
 
 	appendByte(byte) {
-		if (this.byteArray.length - 1 === this.index) {
-			const oldArray = this.byteArray; 
-
-			const newLength = oldArray.length * 2;
-			const newArray = new Uint8Array(newLength);
-
-			newArray.set(oldArray);
-
-			this.byteArray = newArray;
+		const byteArraySpaceFull = this.byteArray.length - 1 === this.index
+		if (byteArraySpaceFull) {
+			this.increaseByteArraySize();
 		};
 
-		this.byteArray[this.index] = byte;
-		this.index++;
+		this.setByteAtCurrentIndex(byte);
 	}
 
 	appendBytes(bytes) {
@@ -341,33 +356,17 @@ class MsgpackEncoder {
 	}
 }
 
-console.log(new MsgpackEncoder().encode(null));
-console.log(new MsgpackEncoder().encode(true));
-console.log(new MsgpackEncoder().encode(false));
-console.log(new MsgpackEncoder().encode(12));
-console.log(new MsgpackEncoder().encode(-12));
-console.log(new MsgpackEncoder().encode(1111111));
-console.log(new MsgpackEncoder().encode(-1111111));
-console.log(new MsgpackEncoder().encode(10.1234));
-console.log(new MsgpackEncoder().encode(255));
-console.log(new MsgpackEncoder().encode(10000000000.1234));
-console.log(new MsgpackEncoder().encode(214748324.128));
-console.log(new MsgpackEncoder().encode(-214748324.128));
-console.log(new MsgpackEncoder().encode("test"));
-console.log(new MsgpackEncoder().encode(new Uint8Array([1, 2, 3, 4])));
-console.log(new MsgpackEncoder().encode(['string', 123, 'lol', -999999.999, 1111111111, ['a', 'b', 111]]));
+class MsgpackDecoder {
+}
 
-const b = {};
 const a = {
 	name: 'Adam',
 	age: 40,
-	b: b,
 	info: {
 		birthyear: 1990,
 		cum: ['cum', 1, -1111.999]
 	}
 };
-b.a = a;
 
-console.log(new MsgpackEncoder().encode(b));
+console.log(new MsgpackEncoder().encode(a));
 
